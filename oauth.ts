945bab992d190e29a21734ac17961c6fd97bdac5
test3207 <test3207@outlook.com>
2021-11-07 06:25:12 +0800

    feat: init project

---
+++ oauth.ts
@@ -0,0 +1,199 @@
+import http from 'http';
+import https from 'https';
+import url, { URL } from 'url';
+import querystring from 'querystring';
+import { config } from './config';
+
+export const enum Provider {
+    github = 'github',
+}
+
+export const enum Env {
+    local,
+    test,
+    staging,
+    prod,
+}
+
+const oauthProviders = {
+    github: {
+        login: `https://github.com/login/oauth/authorize?client_id=${config.githubId}&redirect_uri=REDIRECT_URI&scope=user&state=STATE`,
+        accessToken: `https://github.com/login/oauth/access_token?client_id=${config.githubId}&client_secret=${config.githubSecret}&code=CODE&redirect_uri=REDIRECT_URI`,
+        getUser: `https://api.github.com/user`
+    },
+};
+
+const routes:{[route:string]:(req:http.IncomingMessage, res:http.ServerResponse, next:Function) => void} = {
+    '/entry': (req, res, next) => {
+        if (!req.url) {
+            return next(new Error('Unknow url'));
+        }
+        const query = querystring.parse(url.parse(req.url!).query!)
+        let targetUrl:string;
+        switch (query.provider) {
+            case Provider.github:
+                targetUrl = oauthProviders.github.login;
+                break;
+            default:
+                return next(new Error('unknow provider'));
+        }
+        targetUrl = targetUrl.replace('REDIRECT_URI', encodeURI(config.oauthUrl + 'callback')).replace('STATE', [query.provider, query.env].toString()); // wechat remove all " here so have to be so ugly;
+        console.log(targetUrl);
+        res.writeHead(302, {'Location': targetUrl});
+        res.end();
+    },
+    '/callback': async (req, res, next) => {
+        if (!req.url) {
+            res.writeHead(200);
+            res.end(`
+                <p>Unknow url'</p>
+            `);
+            return;
+            // return next(new Error('Unknow url'));
+        }
+        const query = querystring.parse(url.parse(req.url!).query!)
+        if (!query.code) {
+            res.writeHead(200);
+            res.end(`
+                <p>unauthorized</p>
+            `);
+            return;
+            // return next(new Error('unauthorized'));
+        }
+        console.log(query);
+        console.log(decodeURIComponent(query.state as string));
+        const [ provider, env ] = (query.state as string).split(',');
+        let atUrl:string;
+        switch (provider) {
+            case Provider.github:
+                atUrl = oauthProviders.github.accessToken;
+                break;
+            default:
+                res.writeHead(200);
+                res.end(`
+                    <p>unknow provider</p>
+                `);
+                return;
+                // return next(new Error('unknow provider'));
+        }
+        atUrl = atUrl.replace('CODE', query.code as string).replace('REDIRECT_URI', encodeURI(config.oauthUrl + 'callback'));
+        let oauthInfo:any = await request(
+            atUrl,
+            provider === Provider.github
+            ? 'POST' : 'GET'
+        );
+
+        res.writeHead(200);
+        res.end(`
+            <p>${JSON.stringify(oauthInfo)}</p>
+        `);
+        // res.writeHead(302, {'Location': backendUrl[env] + `?oauthInfo=${JSON.stringify(oauthInfo)}&state=${query.state}`});
+        // res.end();
+    },
+    '/health_check': (_, res) => {
+        res.writeHead(200);
+        res.end();
+    },
+    '/test': (req, res, next) => {
+        const query = querystring.parse(url.parse(req.url!).query!)
+        res.writeHead(200);
+        res.end(`
+            <!DOCTYPE html>
+                <script>
+                    function openOauth() {
+                        var oauthWindow = window.open('${config.oauthUrl}entry?env=${Env.staging}&provider=${query.provider || Provider.github}',
+                        'oauth_page',
+                        'height=580, width=600, top=200, left=300, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no'
+                        );
+                        var listener = (e) => {
+                            console.log('in listener')
+                            console.log(e)
+                            window.removeEventListener('message', listener)
+                        };
+                        window.addEventListener('message', listener, false);
+                    }
+                </script>
+            <button onclick="openOauth()">
+                click to login
+            </button>
+        `);
+    },
+    '/getUser': async (req, res, next) => {
+        const query = querystring.parse(url.parse(req.url!).query!)
+        // if (!query.timestamp || !query.token || !query.provider) {
+        //     return next(new Error('Invalid params'));
+        // }
+        const { timestamp, token, provider, access_token, openid } = query;
+        // if (Math.abs(Number(timestamp) - Number(new Date())) > config.expireTime * 1000) {
+        //     return next(new Error('Outdated'));
+        // }
+        // if (md5(timestamp + config.oauthSecret) !== token) {
+        //     return next(new Error('Auth failed'));
+        // }
+        const base = oauthProviders[provider as string];
+        if (!base) {
+            return next(new Error('Unknow provider'));
+        }
+        const result = await request(base.getUser, 'GET', {
+            'Authorization': access_token,
+        });
+        console.log(result);
+        res.writeHead(200);
+        res.end(result);
+    },
+};
+
+const request = async (targetUrl:string, method='GET', headers?:http.OutgoingHttpHeaders) => {
+    console.log('requesting:', targetUrl);
+    return await new Promise((resolve, reject) => {
+        try {
+            https.request(encodeURI(targetUrl), { method, headers }, (oauthRes) => {
+                const chunkList:any[] = [];
+                oauthRes.on('error', (e) => {
+                    reject(e);
+                });
+                oauthRes.on('data', (chunk) => {
+                    chunkList.push(chunk);
+                });
+                oauthRes.on('end', () => {
+                    resolve(Buffer.concat(chunkList).toString());
+                });
+            }).on('error', (e) => {
+                reject(e);
+            }).end();
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+http.createServer(async (req, res) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} "${req.connection.remoteAddress}" "${req.headers['user-agent']}"`);
+    if (!req.url) {
+        res.writeHead(400);
+        return res.end('url cannot be empty');
+    }
+    const { pathname } = url.parse(req.url!)!
+    if (pathname) {
+        const names = /^\/[\w|_|-|\n]*/.exec(pathname);
+        if (!names) {
+            fail();
+            return;
+        }
+        const route = names[0];
+        const routeHandler = routes[route];
+        if (routeHandler) {
+            try {
+                return routeHandler(req, res, fail);
+            } catch (e) {
+                fail(e as Error);
+            }
+        }
+    } else {
+        fail();
+    }
+    function fail (e?:Error) {
+        res.writeHead(404);
+        res.end(e ? e.message : 'unknow error');
+    }
+}).listen(config.oauthPort);
---
+++ sampleConfig.ts
@@ -0,0 +1,17 @@
+export const config = {
+    oauthPort: '9904',
+    wechatAppid: 'xxx',
+    wechatAppsecret: 'xxx',
+    qqAppid: 'xxx',
+    qqAppsecret: 'xxx',
+    weiboAppid: '',
+    weiboAppsecret: '',
+    oauthUrl: 'https://auth.xxx.com/',
+    oauthSecret: 'you name it',
+    expireTime: 10,
+};
+
+export const backendUrl = {
+    development: 'https://local.test3207.com',
+    production: 'https://oauth.test3207.cn/',
+};
